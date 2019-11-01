8052a2f4bf7553926931d4887bdb2c492197280e
EvertonHimself <evertondeoliveira@gmail.com>
2019-10-31 22:14:41 -0300

    Create a simple move script
    Created a simple movement script for a ball.

---
+++ scripts/Steve.gd
@@ -0,0 +1,41 @@
+extends KinematicBody
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var playerName = "player"
+var velocity = Vector3(0,0,0)
+var SPEED = 20
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	#print("Hello, Godot!")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	#print("Hello");
+	#pass
+	#print(playerName)
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+	else:
+		velocity.x = lerp(velocity.x, 0, 0.1)
+	
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+		velocity.z = SPEED
+	elif Input.is_action_pressed("ui_up"):
+		velocity.z = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.z = SPEED
+	else:
+		velocity.z = lerp(velocity.z, 0, 0.1)
+		
+	move_and_slide(velocity)
